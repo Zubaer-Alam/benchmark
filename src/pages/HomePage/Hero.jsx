900f1e89875024159beba47ad3539bebbcd1caf3
Saidul Arefin <saidularefin8@gmail.com>
2024-04-02 19:25:18 +0600

    fixed homepage parallax

--- src/pages/HomePage/Hero.jsx
+++ src/pages/HomePage/Hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
       }}
     >
       <div
-        className="bg-[url('../public/images/background.jpg')]  w-full h-screen  relative  overflow-hidden block z-10  bg-cover bg-no-repeat bg-top  before:content-['']   before:absolute    before:inset-0  before:block before:bg-gradient-to-b   before:h-56 before:from-black before:to-transparent before:opacity-100 before:z-[-5] ms:mb-10 md:mb-36 lg:mb-36"
+        className="bg-[url('../public/images/background.jpg')]  w-full h-screen  relative  overflow-hidden block z-10  bg-fixed bg-no-repeat bg-top  before:content-['']   before:absolute    before:inset-0  before:block before:bg-gradient-to-b   before:h-56 before:from-black before:to-transparent before:opacity-100 before:z-[-5] ms:mb-10 md:mb-36 lg:mb-36"
         id="home"
       >
       {/* <div className="bg-[url('../public/images/background.jpg')] w-full h-screen relative overflow-hidden block z-10 bg-cover bg-no-repeat bg-top before:content-[''] before:absolute before:inset-0 before:block before:bg-gradient-to-t before:h-80 before:from-black before:to-transparent before:opacity-120 before:z-[-5] after:content-[''] after:absolute after:inset-0 after:block before:top-[700px] after:bg-gradient-to-t after:h-56 after:from-transparent after:to-black after:opacity-100 after:z-[-5] ms:mb-10 md:mb-36 lg:mb-36" id="home">
